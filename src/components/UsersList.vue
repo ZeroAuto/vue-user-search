f89e2eb9b926a097990e8bfc04b1bdc3b5093bfb
Mike James <mikejames386@gmail.com>
2024-03-05 17:53:22 -0600

    okay it's working as expected now

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -3,14 +3,14 @@
     <div class="max-w-md mx-auto bg-gray-800 p-5 rounded-lg shadow-md">
       <div class="mb-4">
         <input
-          onChange={handleInputChange}
+          v-model="state.searchTerm"
           type="text"
           placeholder="Search users..."
           class="w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         />
       </div>
       
-      <div v-if="loading">
+      <div v-if="state.loading">
         <div
           class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
           role="status">
@@ -22,7 +22,7 @@
       </div>
       <ul v-else>
         <li
-          v-for="(user, index) in users"
+          v-for="(user, index) in renderedUsers"
           :key="index"
           class="flex items-center py-2 border-b border-gray-700"
         >
@@ -38,35 +38,41 @@
   </div>
 </template>
 
-<script>
-import { onMounted, ref } from 'vue';
-export default {
-  name: 'UsersList',
+<script setup>
+import {
+  computed,
+  onMounted,
+  reactive,
+} from 'vue';
 
-  setup() {
-    const users = ref([]);
-    const loading = ref(false);
+const state = reactive({
+  users: [],
+  loading: false,
+  searchTerm: '',
+});
 
-    const loadUsers = async () => {
-      loading.value = true;
-      try {
-        const res = await fetch('https://api.github.com/users');
-        console.log(res);
-        const data = await res.json();
-        users.value = data;
-      } finally {
-        loading.value = false;
-      }
-    };
+const loadUsers = async () => {
+  state.loading = true;
+  try {
+    const res = await fetch('https://api.github.com/users');
+    const data = await res.json();
+    state.users = data;
+  } finally {
+    state.loading = false;
+  }
+};
 
-    onMounted(loadUsers);
+const renderedUsers = computed(() => {
+  if (state.searchTerm.length > 0) {
+    return state.users.filter((user) => {
+      return state.searchTerm.toLowerCase().split(' ').every((v) => user.login.toLowerCase().includes(v));
+    });
+  }
+  return state.users;
+});
 
-    return {
-      users,
-      loading,
-    }
-  },
-}
+onMounted(loadUsers);
+
 </script>
 
 <style>
